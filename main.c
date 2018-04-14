e0a0ce21fa9d245cff500ad901629d4aedca8cec
Harrison <lbr0452000@naver.com>
2018-04-14 22:06:21 +0900

    Program was edited to end if all problems were printed

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 void printLine(FILE *f, int line)
 {
@@ -53,43 +54,62 @@ int linesOfFile(FILE *f)
 	return lines;
 }
 
-int numberOfProblem(FILE *f)
+int *mixedOrderOfProblem(int num)
 {
-	return (linesOfFile(f) / 3) - 1;
-}
+	static int *resultArr;
+	resultArr = (int*)malloc(sizeof(int) * num);
 
-int randomProblemNumber(FILE *f)
-{
+
+	for (int i = 0;i < num;i++)
+	{
+		resultArr[i] = i;
+	}
+
 	srand(time(NULL));
-	return (rand() % numberOfProblem(f)) + 1;
+	int swapNum;
+	int temp;
+	for (int i = 0;i < num;i++)
+	{
+		swapNum = rand() % num;
+
+		temp = resultArr[i];
+		resultArr[i] = resultArr[swapNum];
+		resultArr[swapNum] = temp;
+	}
+	return resultArr;
 }
 
 int main(void)
 {
 	char fileName[32];
-
 	printf("ProblemMaker\n시험문제를 무작위로 보여줍니다.\n제작자 lbr0452000\n\n");
 	printf("문제를 제출할 파일 이름을 입력하세요.\n(예시 : 문학과예술의사회사.txt)\n");
 	scanf("%s", fileName);
 	getchar();
 	system("cls");
-
 	FILE *f = fopen(fileName, "r");
+
 	char trash;
-	while (1)
+	int numberOfProblem = linesOfFile(f) / 3;
+	int *orderOfProblem = mixedOrderOfProblem(numberOfProblem);
+
+	for (int i = 0;i < numberOfProblem;i++)
 	{
-		printLine(f, randomProblemNumber(f) * 3);
+		printLine(f, orderOfProblem[i] * 3);
 		while (1)
 		{
 			scanf("%c", &trash);
 			if (trash == '\n')
 				break;
 		}
-		
+
 		printf("정답 : ");
 		printNextLine(f);
 		printf("\n\n");
 	}
+
+	printf("모든 문제를 출력했습니다. 아무 키나 누르면 프로그램을 종료합니다.");
+	getch();
 	
 	return 0;
 }
